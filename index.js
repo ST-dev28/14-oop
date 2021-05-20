6aee35a6af97c00cbf5598fdd862b1f6ba3fa215
SandraT <sndrbltr@gmail.com>
2021-05-20 15:51:22 +0000

    Pet extends

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const Dog = require('./js/Dog'); 
+const Dog = require('./js/Dog');
+const Cat = require('./js/Cat');
+const Rabbit = require('./js/Rabbit');
 
 const rexas = new Dog('Rex', 'brown');
-const spaikas = new Dog('Spike', 'white');
+rexas.voice();
+
+const rainis = new Cat('Rainis', 'golden');
+rainis.voice();
+
+const benas = new Rabbit('Benas', 'black');
+benas.voice();
 
 
+/*
 console.log(rexas);
 console.log(spaikas);
+console.log(rainis);
+console.log(pukis);
 
 spaikas.voice();
 rexas.voice();
 spaikas.voice();
-rexas.voice();
 spaikas.voice();
-spaikas.voice();
+rainis.voice();
+pukis.voice();
+*/
+
 
 
